fe54fb9033dccf393293585ba754bda64d67a780
kinheiw01 <kin.hei.working@gmail.com>
2024-05-03 16:09:29 +1000

    Yeet

---
+++ S0_1_HelloWorld.py
@@ -0,0 +1,2 @@
+# In Python, we use print() function to print things out. Execute the following code:
+print("Hello World!")
---
+++ Section0.py
@@ -0,0 +1,5 @@
+# Welcome to Chapter 8: Algorithm. Before starting, let's get used to the environment of Python.
+# If you have not installed Python (optional: VSCode), you need to do so by refering to the LMS Canvas.
+# You should be able to track the structure of the file by looking at the name of file
+# E.g. S0_1_HelloWorld.py: This means Section 0, 1st file of Section 0, filename is HelloWorld
+# In Section 0, we will be learning mainly some basic needs of Python to continue on. 
